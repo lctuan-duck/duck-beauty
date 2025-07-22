ae6c16cb8b424e03b0b4b2cb68546abe214aa98b
gld-tuanle <tuanle@golivedev.com>
2025-07-22 22:59:25 +0700

    style(core): style of component core loading-screen

--- app/components/loading-screen/index.vue
+++ app/components/loading-screen/index.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    isLoading?: boolean;
+  }>(),
+  {
+    isLoading: false,
+  }
+);
+</script>
+
 <template>
-  <DuckBox
-    class="size-dvh absolute top-0 left-0 right-0 bottom-0 z-[9999] bg-[var(--ui-bg)] flex items-center justify-center"
-  >
-  </DuckBox>
+  <Transition name="fade" mode="out-in">
+    <DuckBox
+      v-if="isLoading"
+      class="size-dvh fixed top-0 left-0 right-0 bottom-0 z-[9999] bg-[var(--ui-bg)] flex items-center justify-center"
+    >
+    </DuckBox>
+  </Transition>
 </template>
